39f1d128afbf61cea357c7725b0fdc04c10d3bce
James Nguyen <james.nguyenn96@gmail.com>
2019-11-05 22:33:58 +1100

    Create function to read data from the json file

--- Exercise-2/dist/js/index.js
+++ Exercise-2/dist/js/index.js
@@ -1,4 +1,9 @@
 const xhttp = new XMLHttpRequest();
 
-xhttp.open("GET", "data.json", true);
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.querySelector(".p-data").textContent = this.responseText;
+  }
+};
+xhttp.open("GET", "../data.json", true);
 xhttp.send();
