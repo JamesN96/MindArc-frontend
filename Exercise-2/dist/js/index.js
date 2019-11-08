31988924e1773565475f28cc97d7ba5e1d11c8b0
James Nguyen <james.nguyenn96@gmail.com>
2019-11-08 16:35:49 +1100

    Edit eventlistener functions to hide other divs

--- Exercise-2/dist/js/index.js
+++ Exercise-2/dist/js/index.js
@@ -6,26 +6,43 @@ const tab1 = document.querySelector("#tab-1");
 const tab2 = document.querySelector("#tab-2");
 const tab3 = document.querySelector("#tab-3");
 const tab4 = document.querySelector("#tab-4");
+const divs = document.querySelectorAll(".data");
 
 // Event listeners
 
 tab1.addEventListener("click", function(e) {
   e.target.value = 0;
+  // Hide other divs
+  tab2.nextElementSibling.style.display = "";
+  tab3.nextElementSibling.style.display = "";
+  tab4.nextElementSibling.style.display = "";
   json(e);
 });
 
 tab2.addEventListener("click", function(e) {
   e.target.value = 1;
+  // Hide other divs
+  tab1.nextElementSibling.style.display = "";
+  tab3.nextElementSibling.style.display = "";
+  tab4.nextElementSibling.style.display = "";
   json(e);
 });
 
 tab3.addEventListener("click", function(e) {
   e.target.value = 2;
+  // Hide other divs
+  tab1.nextElementSibling.style.display = "";
+  tab2.nextElementSibling.style.display = "";
+  tab4.nextElementSibling.style.display = "";
   json(e);
 });
 
 tab4.addEventListener("click", function(e) {
   e.target.value = 3;
+  // Hide other divs
+  tab1.nextElementSibling.style.display = "";
+  tab2.nextElementSibling.style.display = "";
+  tab3.nextElementSibling.style.display = "";
   json(e);
 });
 
@@ -33,19 +50,22 @@ tab4.addEventListener("click", function(e) {
 
 function json(e) {
   if (xhttp.readyState == 4 && xhttp.status == 200) {
+    // Create an object
     let myObj = JSON.parse(xhttp.responseText);
     if (e.target.nextElementSibling.style.display == "") {
+      // Loop through object
       for (let i = 0; i < myObj.length; i++) {
         // Display hidden Div
         e.target.nextElementSibling.style.display = "block";
-        // Select the h1 inside of the target tab element
+        // Select the h1 inside of the target tab element and display title data
         e.target.nextElementSibling.firstElementChild.innerHTML =
           myObj[e.target.value].title;
-        // Select the p element inside of the target tab element
+        // Select the p element inside of the target tab element and display content data
         e.target.nextElementSibling.lastElementChild.innerHTML =
           myObj[e.target.value].content;
       }
     } else {
+      // Hide div
       e.target.nextElementSibling.style.display = "";
     }
   }
@@ -56,6 +76,7 @@ function json(e) {
 xhttp.onload = function() {
   let myObj = JSON.parse(xhttp.responseText);
   if (xhttp.readyState == 4 && xhttp.status == 200) {
+    tab1.nextElementSibling.style.display = "block";
     document.querySelector(".title-data").innerHTML = myObj[0].title;
     document.querySelector(".p-data").innerHTML = myObj[0].content;
   }
